cbf6961c63ce50bca77ba801641216cf3cfd3359
Filipp Balakin <filipp@balakin.ru>
2021-06-19 19:56:47 +0300

    replace libs with elvui/weakauras versions

--- libs/AceGUI-3.0/widgets/AceGUIWidget-Button.lua
+++ libs/AceGUI-3.0/widgets/AceGUIWidget-Button.lua
@@ -13,12 +13,6 @@ local pairs = pairs
 local _G = _G
 local PlaySound, CreateFrame, UIParent = PlaySound, CreateFrame, UIParent
 
-local wowMoP
-do
-	local _, _, _, interface = GetBuildInfo()
-	wowMoP = (interface >= 50000)
-end
-
 --[[-----------------------------------------------------------------------------
 Scripts
 -------------------------------------------------------------------------------]]
@@ -57,7 +51,7 @@ local methods = {
 			self:SetWidth(self.text:GetStringWidth() + 30)
 		end
 	end,
-	
+
 	["SetAutoWidth"] = function(self, autoWidth)
 		self.autoWidth = autoWidth
 		if self.autoWidth then
@@ -80,7 +74,7 @@ Constructor
 -------------------------------------------------------------------------------]]
 local function Constructor()
 	local name = "AceGUI30Button" .. AceGUI:GetNextWidgetNum(Type)
-	local frame = CreateFrame("Button", name, UIParent, wowMoP and "UIPanelButtonTemplate" or "UIPanelButtonTemplate2")
+	local frame = CreateFrame("Button", name, UIParent, "UIPanelButtonTemplate2")
 	frame:Hide()
 
 	frame:EnableMouse(true)
